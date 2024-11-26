1adf7664adeb8a3585a1e4b1a6d827a30f4bfee3
NinoDulay <altisjessienino18@gmail.com>
2024-11-26 14:31:22 +0800

    Templates in Django

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -3,14 +3,33 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+# Sample dynamic data
+posts = [
+    {
+        "author": "NinoDulay",
+        "title": "Blog Post 1",
+        "content": "First content ever!",
+        "date_posted": "August 20, 2020"
+    },
+    {
+        "author": "NinoDulay",
+        "title": "Blog Post 2",
+        "content": "Second content ever!",
+        "date_posted": "August 20, 2021"
+    }
+]
+
 # Create your views here.
 
 ## Handle traffic from homepage of our blog
 def home(request):
-    
-    return render(request, 'blog/home.html')
-    
+    context = {
+        "posts": posts
+    }
+    return render(request, 'blog/home.html', context)
+
+    #return render(request, 'blog/home.html')    
     # return HttpResponse('<h1>Blog Home</h1>')
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
